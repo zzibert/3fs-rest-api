708d8ab841fc2f658b36d2d5dc58c44000b1f9b2
Zan Zibert <zan_zibert@hotmail.com>
2020-08-13 10:50:47 +0200

    created the group data api structure

--- data/groups.go
+++ data/groups.go
@@ -19,3 +19,33 @@ type Group struct {
 	// max length: 255
 	Name string `json:"name"`
 }
+
+// Groups defines a slice of Group
+type Groups []*Group
+
+// GetGroups returns all groups from the database
+func GetGroups() Groups {
+
+}
+
+// GetGroupById returns a single group with the specified id
+// If a group is not found this func returns a GroupNotFound error
+func GetGroupById(id int) (*Group, error) {
+
+}
+
+// UpdateGroup replaces a group with the given item
+// If a group is not found this func returns a GroupNotFound error
+func UpdateGroup(g Group) error {
+
+}
+
+// AddGroup adds a group to the database
+func AddGroup(g Group) {
+
+}
+
+// DeleteGroup deletes a group from the database
+func DeleteGroup(id int) error {
+
+}
