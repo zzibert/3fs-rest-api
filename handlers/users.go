de0e4c771643522c0bdb3d39814693ed093f68b7
Zan Zibert <zan_zibert@hotmail.com>
2020-08-13 13:11:05 +0200

    adding listAll, listSingle, Update hadlers for user

--- handlers/users.go
+++ handlers/users.go
@@ -1,1 +1,110 @@
 package handlers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/zzibert/3fs-rest-api/data"
+)
+
+// Users handler for getting and updating users
+type Users struct {
+	l *log.Logger
+}
+
+// NewUsers returns a new users handler with the given logger
+func NewUsers(l *log.Logger) *Users {
+	return &Users{l}
+}
+
+// ErrInvalidUserPath is an error when user path is not valid
+var ErrInvalidUserPath = fmt.Errorf("Invalid path, path should be /users/[id]")
+
+// GenericError is a generic error message
+type GenericError struct {
+	Message string `json:"message"`
+}
+
+// getUserId returnes the user Id from the URL
+// panics if it cannot convert the id into an integer
+func getUserId(r *http.Request) int {
+	// parse the user id from the url
+	vars := mux.Vars(r)
+
+	// convert the id into an integer
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		panic(err)
+	}
+
+	return id
+}
+
+// swagger:route GET /users users ListUsers
+// Returns a list of users from the database
+// responses:
+//  200: UsersResponse
+
+// ListAll handles GET requests and returns all current users
+func (u *Users) ListAll(rw http.ResponseWriter, r *http.Request) {
+	u.l.Println("Get all users")
+
+	users := data.GetUsers()
+
+	err := data.ToJSON(users, rw)
+	if err != nil {
+		u.l.Println("error encoding users")
+	}
+}
+
+// swagger:route GET /users/{id} users ListUser
+// Returns a single user from the database
+// responses:
+//  200: userResponse
+//  404: errorResponse
+
+// ListSingle handles GET requests with id
+func (u *Users) ListSingle(rw http.ResponseWriter, r *http.Request) {
+	id := getUserId(r)
+
+	u.l.Println("get user id", id)
+
+	user, err := data.GetUserById(id)
+
+	switch err {
+
+	}
+}
+
+// swagger:route PUT /users users updateUser
+// update an user
+//
+// responses:
+//  201: noContentResponse
+//  404: errorResponse
+
+// Update handles PUT to update users
+func (u *Users) Update(rw http.ResponseWriter, r *http.Request) {
+
+	// create the user from the request body
+	var user data.User
+	err := data.FromJSON(user, r.Body)
+	if err != nil {
+		u.l.Println("Error couldnt parse user from request body", err)
+
+		rw.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = data.UpdateUser(user)
+	if err == nil {
+		u.l.Println("Error user not found", user)
+
+		rw.WriteHeader(http.StatusNotFount)
+		return
+	}
+
+	rw.WriteHeader(http.StatusNoContent)
+}
