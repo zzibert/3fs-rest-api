b8c32117cbaa2bfb0bebacfd6f3eda31cdc38da3
Zan Zibert <zan_zibert@hotmail.com>
2020-08-16 12:30:12 +0200

    starting to write tests

--- main_test.go
+++ main_test.go
@@ -1,15 +1,21 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 
+	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
+	"github.com/zzibert/3fs-rest-api/handlers"
+	. "gopkg.in/check.v1"
 )
 
-var db *sql.DB
+var db *gorm.DB
+var l *log.Logger
 
 func TestMain(m *testing.M) {
 	initialize()
@@ -20,8 +26,8 @@ func TestMain(m *testing.M) {
 
 func initialize() {
 	connection := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", "localhost", "5433", "zanzibert", "nekineki", "test")
-
-	db, _ = sql.Open("postgres", connection)
+	l = log.New(os.Stdout, "3fs-rest-api", log.LstdFlags)
+	db, _ = gorm.Open("postgres", connection)
 }
 
 func clearDB() {
@@ -29,4 +35,30 @@ func clearDB() {
 	db.Exec("alter sequence users_id_seq restart with 1")
 	db.Exec("delete from groups")
 	db.Exec("alter sequence groups_id_seq restart with 1")
+}
+
+type PostTestSuite struct {
+	mux          *http.ServeMux
+	writer       *httptest.ResponseRecorder
+	groupHandler *handlers.Groups
+}
+
+func init() {
+	Suite(&PostTestSuite{})
+}
+
+func Test(t *testing.T) { TestingT(t) }
+
+func (s *PostTestSuite) SetUpTest(c *C) {
+	s.mux = http.NewServeMux()
+	s.groupHandler = handlers.NewGroups(l, db)
+	s.mux.HandleFunc("/groups", s.groupHandler.ListAll)
+	s.writer = httptest.NewRecorder()
+}
+
+func (s *PostTestSuite) TestHandleGet(c *C) {
+	request, _ := http.NewRequest("GET", "/groups", nil)
+	s.mux.ServeHTTP(s.writer, request)
+
+	c.Check(s.writer.Code, Equals, 200)
 }
